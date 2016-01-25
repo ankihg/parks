5db8fd604a7d17989654f8579360f83ded11ec26
cacizi41 <qiangwenjing@gmail.com>
2016-01-25 14:48:09 -0800

    seattlelatlng/markerview

---
+++ scripts/markerView.js
@@ -0,0 +1,25 @@
+var locations = [Park.lat,Park.lng];
+
+    var map = new google.maps.Map(document.getElementById('googleMap'), {
+      zoom: 10,
+      center: new google.maps.LatLng(47.53, -122.30),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+
+    for (i = 0; i < locations.length; i++) {
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(Park.lat, Park.lng),
+        map: map
+      });
+
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i][0]);
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+    }
