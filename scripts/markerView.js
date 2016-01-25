1bc29777e7bb01ad40ee20f0147a7af194a59a6d
cacizi41 <qiangwenjing@gmail.com>
2016-01-25 14:34:01 -0800

    push to pull

--- scripts/markerView.js
+++ scripts/markerView.js
@@ -1,11 +1,4 @@
-
-var locations = [
-      ['Bondi Beach', -33.890542, 151.274856, 4],
-      ['Coogee Beach', -33.923036, 151.259052, 5],
-      ['Cronulla Beach', -34.028249, 151.157507, 3],
-      ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
-      ['Maroubra Beach', -33.950198, 151.259302, 1]
-    ];
+var locations = [this.name,this.lat,this.lng];
 
     var map = new google.maps.Map(document.getElementById('googleMap'), {
       zoom: 10,
@@ -18,14 +11,14 @@ var locations = [
     var marker, i;
 
     for (i = 0; i < locations.length; i++) {
-      marker = new google.maps.Marker({
-        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        marker = new google.maps.Marker({
+        position: new google.maps.LatLng(this.lat, this.lng),
         map: map
       });
 
       google.maps.event.addListener(marker, 'click', (function(marker, i) {
         return function() {
-          infowindow.setContent(locations[i][0]);
+          infowindow.setContent(Park.name);
           infowindow.open(map, marker);
         }
       })(marker, i));
