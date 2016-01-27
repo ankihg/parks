73633c774e1f4be97d51e9d4dc176a61442de00e
ankihg <hagelin.annika@gmail.com>
2016-01-27 13:36:06 -0800

    more weather syling

--- scripts/weather.js
+++ scripts/weather.js
@@ -7,9 +7,9 @@ $(document).ready(function() {
     success: function(weather) {
       // html = '<h6>'+weather.city+' '+weather.forecast[0].date+'</h6>';
       var html = "";
-      html += '<img style="float:right; width: 39%; height: 35%" src="./media/weather/'+weather.code+'.png">';
+      html += '<img style="float:right; width: 39%; height: 35%; margin-right: 10%;" src="./media/weather/'+weather.code+'.png">';
       html += '<br><h6>'+weather.currently+'<br>Temp: '+weather.temp+'&deg;'+weather.units.temp+'</h6>';
-      html += '<div><small>Sunrise: '+weather.sunrise+' &nbsp; | &nbsp; Sunset: '+weather.sunset+'</small></div>';
+      html += '<div style="line-height:0%;"><small>Sunrise: '+weather.sunrise+' &nbsp; | &nbsp; Sunset: '+weather.sunset+'</small></div>';
       console.log("workinghere");
 
       $("#weather").html(html);
