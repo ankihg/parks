961c8153ea96bb0e68c559b63ccc9815a6d6e7cf
David Livingstone <david@david.tl>
2016-01-27 15:40:15 -0800

    nav links hide when window < 1070px

--- scripts/weather.js
+++ scripts/weather.js
@@ -7,8 +7,8 @@ $(document).ready(function() {
     success: function(weather) {
       // html = '<h6>'+weather.city+' '+weather.forecast[0].date+'</h6>';
       var html = "";
-      html += '<img style="float:right; width: 39%; height: 35%; margin-right: 10%;" src="/media/weather/'+weather.code+'.png">';
-      html += '<br><h6>'+weather.currently+'<br>Temp: '+weather.temp+'&deg;'+weather.units.temp+'</h6>';
+      html += '<img style="width: 82px; height: 82px;" src="/media/weather/'+weather.code+'.png"></img>';
+      html += '<h6 style="width: 82px; height: 82px">'+weather.currently+'<br>'+weather.temp+'&deg;'+weather.units.temp+'</h6>';
       html += '<div style="line-height:0%;"><small>Sunrise: '+weather.sunrise+' &nbsp; | &nbsp; Sunset: '+weather.sunset+'</small></div>';
       console.log("workinghere");
 
