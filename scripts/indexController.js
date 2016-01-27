318bb79a5019942c185176cb29e7b2b205741c11
ankihg <hagelin.annika@gmail.com>
2016-01-27 15:51:40 -0800

    edited about-is

--- scripts/indexController.js
+++ scripts/indexController.js
@@ -11,10 +11,11 @@
 
   function ui() {
     window.scrollTo(0, 0);
-    
+
     $('#park-comments').hide();
     $('#events').hide();
     $('#park-page').hide();
+    $('#about-us').hide();
 
     $('#park-info').show();
     $('#googleMap').show();
